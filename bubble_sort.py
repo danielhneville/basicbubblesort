effd9a51f81040a7d6bee42900a4a8a3dcc664c9
Daniel Heffner <cdanielheffner@gmail.com>
2016-08-02 09:52:07 -0500

    added counter for number of times the loop ran and timer for how long the sorting function took

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,11 +1,25 @@
 import random
+import time
+
 x = []
 for i in range(100):
 	x.append(int(round(random.random()*10000)))
 
+def timer(func):
+	def wrapper(*arg, **kw):
+		t1 = time.time()
+		res = func(*arg, **kw)
+		t2 = time.time()
+		print (t2-t1)*1000, "ms"
+		return (t2 - t1), res, func.__name__
+	return wrapper
+
+@timer
 def bubbleSort(x):
 	changes = 1
+	count = 0
 	while changes>0:
+		count += 1
 		changes = 0
 		for i in range(0, len(x)-1):
 			one = x[i]
@@ -13,6 +27,7 @@ def bubbleSort(x):
 			if one > two:
 				x[i], x[i+1] = x[i+1], x[i]
 				changes += 1
-	return x
+	print x
+	print count,"times around the loop"
 
-print bubbleSort(x)
+bubbleSort(x)
